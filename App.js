7ff6e4f6bd38a666a1771068123b37caf51674e9
harang <harang@taling.me>
2023-11-21 05:02:01 +0900

    feat : 코드 정리용 커밋. 레이아웃 작업본 업로드

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
-  Dimensions,
   StyleSheet,
   ScrollView,
+  Alert,
 } from "react-native";
 import {OPEN_WEATHER_API_KEY} from "@env";
 
@@ -13,27 +13,15 @@ const API_KEY = OPEN_WEATHER_API_KEY;
 
 export default function App() {
   const [location, setLocation] = useState(null);
-  const [days, setDays] = useState([]);
-  const [weatherData, setWeatherData] = useState([
-    { date: "2023-11-01", high: 30, low: 20 },
-    { date: "2023-11-02", high: 31, low: 21 },
-    { date: "2023-11-03", high: 32, low: 22 },
-    { date: "2023-11-04", high: 33, low: 23 },
-    { date: "2023-11-05", high: 34, low: 24 },
-    { date: "2023-11-06", high: 35, low: 25 },
-    { date: "2023-11-07", high: 36, low: 26 },
-    { date: "2023-11-08", high: 37, low: 27 },
-    { date: "2023-11-09", high: 38, low: 28 },
-    { date: "2023-11-10", high: 39, low: 29 },
-  ]);
+  const [weatherData, setWeatherData] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
 
-  const getLoaction = async () => {
+  const getLocation = async () => {
     try {
       await Location.requestForegroundPermissionsAsync();
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-      console.log(latitude, longitude);
       return { latitude, longitude };
     } catch (error) {
       Alert.alert("Can't find you.");
@@ -41,87 +29,109 @@ export default function App() {
   };
 
   const getWeather = async () => {
-    console.log("getWeather");
-    const coords = await getLoaction();
+    const coords = await getLocation();
     const { latitude, longitude } = coords;
     const location = await Location.reverseGeocodeAsync(
       { latitude, longitude },
       { useGoogleMaps: false }
     );
-    console.log(location);
     setLocation(location);
 
-    // open weather api
-
-    const url  = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&mode=json&units=metric&lang=kr`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&mode=json&units=metric&lang=kr`;
     console.log(url);
-    const response = await fetch(
-      url
-
-    );
-
+    const response = await fetch(url);
     const json = await response.json();
-    console.log(json);
-    setDays(json.daily);
-
+    setWeatherData(json.list);
+    const currentWeather = weatherData ? weatherData[0] : null;
+    setCurrentWeather(currentWeather);
   };
 
   useEffect(() => {
-    getLoaction();
     getWeather();
   }, []);
 
-
-  const regionString = location ? `${location[0].region} ${location[0].district}` : "Loading...";
+  const regionString = location ? `${location[0]?.region} ${location[0]?.district}` : "Loading...";
   return (
-    <View style={styles.container}>
-      <View style={styles.topSection}>
-        <View style={styles.temperatureSection}>
-          <Text style={styles.temperatureText}>23°C</Text>
-          <Text>{regionString}</Text>
-          <Text>체감 25°C</Text>
-        </View>
-        <View style={styles.weatherIconSection}>
-          <Text>☀️</Text>{/* 날씨 아이콘 대체 */}
-        </View>
-      </View>
-
-      <View style={styles.graphSection}>
-        {/* 그래프 영역 */}
-        <Text>시간별 온도 그래프</Text>
-      </View>
-
-      <ScrollView style={styles.tableSection}>
-        {weatherData.map((item, index) => (
-          <View key={index} style={styles.tableRow}>
-            <Text style={styles.tableCell}>{item.date}</Text>
-            <Text style={styles.tableCell}>{item.high}°C</Text>
-            <Text style={styles.tableCell}>{item.low}°C</Text>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+          <View style={styles.topSection}>
+            <View style={styles.temperatureSection}>
+              <Text style={styles.temperatureText}>{currentWeather?.main?.temp}°C</Text>
+              <Text>{regionString}</Text>
+              <Text>체감 {currentWeather?.main?.feels_like}</Text>
+            </View>
+            <View style={styles.weatherIconSection}>
+              <Text>{currentWeather?.weather[0]?.icon}</Text>
+            </View>
           </View>
-        ))}
+          <View style={styles.graphSection}></View>
+          <ScrollView style={styles.tableSection}>
+            <View style={styles.tableRow}>
+              <Text style={styles.tableCell}>시간</Text>
+              <Text style={styles.tableCell}>날씨</Text>
+              <Text style={styles.tableCell}>강수확률</Text>
+              <Text style={styles.tableCell}>강수량</Text>
+              <Text style={styles.tableCell}>습도</Text>
+              <Text style={styles.tableCell}>풍속</Text>
+            </View>
+            {weatherData && weatherData.map((item, index) => (
+              <View style={styles.tableRow} key={index}>
+                <Text style={styles.tableCell}>{new Date(item.dt * 1000).toLocaleTimeString()}</Text>
+                <Text style={styles.tableCell}>{item.weather[0].description}</Text>
+                <Text style={styles.tableCell}>{item.pop}%</Text>
+                <Text style={styles.tableCell}>{item.rain ? item.rain["3h"] : 0}mm</Text>
+                <Text style={styles.tableCell}>{item.main.humidity}%</Text>
+                <Text style={styles.tableCell}>{item.wind.speed}m/s</Text>
+              </View>
+            ))}
+          </ScrollView>
+          <View style={styles.infoBoxesSection}>
+            <View style={styles.infoBox}>
+              <Text>일출</Text>
+              <Text>06:00</Text>
+            </View>
+            <View style={styles.infoBox}>
+              <Text>일출</Text>
+              <Text>06:00</Text>
+            </View>
+            <View style={styles.infoBox}>
+              <Text>일출</Text>
+              <Text>06:00</Text>
+            </View>
+            <View style={styles.infoBox}>
+              <Text>일출</Text>
+              <Text>06:00</Text>
+            </View>
+          </View>
+          <View style={styles.infoBoxesSection}>
+            <View style={styles.infoBox}>
+              <Text>일출</Text>
+              <Text>06:00</Text>
+            </View>
+          </View>
+        </View>
       </ScrollView>
-
-
-      <View style={styles.infoBoxesSection}>
-        {/* 정보 박스 */}
-        <View style={styles.infoBox}><Text>미세먼지</Text></View>
-        <View style={styles.infoBox}><Text>초미세먼지</Text></View>
-        <View style={styles.infoBox}><Text>자외선 지수</Text></View>
-        <View style={styles.infoBox}><Text>습도</Text></View>
-        <View style={styles.infoBox}><Text>바람</Text></View>
-        <View style={styles.infoBox}><Text>일몰 & 일출</Text></View>
-      </View>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 5,
-    padding: 20,
+  scrollView: {
+    flex: 1,
     backgroundColor: 'white',
   },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  gradient: {
+    width: 300,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  },
   topSection: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 20,
@@ -136,11 +146,11 @@ const styles = StyleSheet.create({
   weatherIconSection: {
     justifyContent: 'center',
     alignItems: 'center',
-    // 아이콘 크기에 따라 조정
     width: 50,
     height: 50,
   },
   graphSection: {
+    flex: 5,
     height: 200,
     backgroundColor: 'gray',
     justifyContent: 'center',
@@ -148,6 +158,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   tableSection: {
+    flex: 5,
     height: 150,
     backgroundColor: 'gray',
     marginBottom: 20,
@@ -163,6 +174,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   infoBoxesSection: {
+    flex: 5,
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
@@ -176,5 +188,3 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
 });
-
-
